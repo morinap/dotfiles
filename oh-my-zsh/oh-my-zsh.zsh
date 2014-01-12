97ba8d8bbf2dd6b767560e38b3b711fddf894105
Andrew Morin <andrew.morin@oracle.com>
2014-01-12 10:46:51 -0500

    Add oh-my-zsh support

---
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -0,0 +1,15 @@
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Use crunch theme
+ZSH_THEME="crunch"
+
+# Use case sensitive mode
+CASE_SENSITIVE="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
