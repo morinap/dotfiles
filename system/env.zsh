81744e6cc9e982cbaf59c700517b495bea187732
Andrew Morin <andrew@andrewmorin.com>
2018-11-19 10:49:39 -0500

    Getting started with vim

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,7 @@
-export EDITOR='subl -w'
+# Only set this if we haven't set $EDITOR up somewhere else previously.
+if [[ "$EDITOR" == "" ]] ; then
+  # Use sublime for my editor.
+  #export EDITOR='subl -w'
+  #export EDITOR='atom'
+  export EDITOR='mvim -f'
+fi
