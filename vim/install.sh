ca87e68c6fb5d18368b2d2d6a5c248afc52a52b1
Andrew Morin <andrew@andrewmorin.com>
2019-01-21 15:23:55 -0500

    Rework some colorscheme/UI settings in VIM
    Also add install script to pull down additional VIM items

---
+++ vim/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p ~/.vim/colors
+curl -fsSL https://raw.githubusercontent.com/vim-scripts/darktango.vim/master/colors/darktango.vim -o ~/.vim/colors/darktango.vim
+echo "Updated darktango vim theme"
+
+curl -fsSL https://raw.githubusercontent.com/Siphalor/vim-atomified/master/colors/atomified.vim -o ~/.vim/colors/atomified.vim
+echo "Updated atomified vim theme"
+
+curl -fsSL https://raw.githubusercontent.com/jsit/disco.vim/master/colors/disco.vim -o ~/.vim/colors/disco.vim
+echo "Updated disco vim theme"
