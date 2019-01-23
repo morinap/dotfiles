b350cdf54db38bd86a31925e9d9b167ac0950a12
Andrew Morin <andrew@andrewmorin.com>
2019-01-23 07:41:32 -0500

    Temporary fix for asdf path issue

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:$ZSH_DOTFILES/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+. $HOME/.asdf/asdf.sh
