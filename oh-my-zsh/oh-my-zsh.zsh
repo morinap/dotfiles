87c0d4b51db9f79f94bf509db2136529537cd2cc
Andrew Morin <andrew@andrewmorin.com>
2019-10-21 11:30:29 -0400

    Add kubectl oh-my-zsh plugin

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -10,6 +10,6 @@ CASE_SENSITIVE="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git zsh-syntax-highlighting)
+plugins=(git zsh-syntax-highlighting kubectl)
 
 source $ZSH/oh-my-zsh.sh
